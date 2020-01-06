2faa5b3895707b68c3edbd7cb26c495242042685
Feeelix92 <felix.maurer@informatik.hs-fulda.de>
2020-01-06 16:54:49 +0100

    Quiz Fragen Screen Antwort Buttons

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -5,7 +5,7 @@ class GetJson extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: DefaultAssetBundle.of(context).load('assets/python.json'),
+      future: DefaultAssetBundle.of(context).loadString('assets/test.json'),
       builder: (context, snapshot) {
         var myInput = json.decode(snapshot.data.toString());
         if (myInput == null) {
@@ -30,6 +30,26 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  Widget antwortButton(option) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 20.0,
+      ),
+      child: MaterialButton(
+        onPressed: () {},
+        child: Text(
+          "Antwort " + option,
+          style: TextStyle(
+            fontSize: 18.0,
+          ),
+          maxLines: 1,
+        ),
+        color: Colors.lime,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,24 +58,42 @@ class _QuizPageState extends State<QuizPage> {
           Expanded(
             flex: 3,
             child: Container(
-              decoration: BoxDecoration(
-                color: Colors.red,
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "Beispiel Frage?",
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
               ),
             ),
           ),
           Expanded(
             flex: 6,
             child: Container(
-              decoration: BoxDecoration(
-                color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  antwortButton("a"),
+                  antwortButton("b"),
+                  antwortButton("c"),
+                  antwortButton("d"),
+                ],
               ),
             ),
           ),
           Expanded(
             flex: 1,
             child: Container(
-              decoration: BoxDecoration(
-                color: Colors.teal,
+              alignment: Alignment.topCenter,
+              child: Center(
+                child: Text(
+                  "30",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
               ),
             ),
           ),
