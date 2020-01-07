1387259a39b64bf27c30262c853ce75580b46150
Feeelix92 <felix.maurer@informatik.hs-fulda.de>
2020-01-07 12:20:39 +0100

    Quiz funktioniert. Ergebnis Seite am Ende des Quiz, mit Feedback und erzieltem Score.

--- lib/screens/quiz_screen.dart
+++ lib/screens/quiz_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_lernquiz_app/screens/result_screen.dart';
 
 class GetJson extends StatelessWidget {
   @override
@@ -8,7 +10,7 @@ class GetJson extends StatelessWidget {
     return FutureBuilder(
       future: DefaultAssetBundle.of(context).loadString('assets/test.json'),
       builder: (context, snapshot) {
-        var myInput = json.decode(snapshot.data.toString());
+        List myInput = json.decode(snapshot.data.toString());
         if (myInput == null) {
           return Scaffold(
             body: Center(
@@ -18,7 +20,7 @@ class GetJson extends StatelessWidget {
             ),
           );
         } else {
-          return QuizPage();
+          return QuizPage(myInput: myInput);
         }
       },
     );
@@ -26,11 +28,104 @@ class GetJson extends StatelessWidget {
 }
 
 class QuizPage extends StatefulWidget {
+
+  var myInput;
+  QuizPage({key, this.myInput}) : super(key: key);
+
   @override
-  _QuizPageState createState() => _QuizPageState();
+  _QuizPageState createState() => _QuizPageState(myInput);
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  var myInput;
+  _QuizPageState(this.myInput);
+
+  Color colortoshow = Colors.blue;
+  Color right = Colors.lime;
+  Color wrong = Colors.red;
+  int punkte = 0;
+  int anzahlFragen = 0;
+  int richtigeAntworten = 0;
+  //Index um die jeweilige Frage bzw. Antowort aus dem Json zu laden.
+  int index = 1;
+  //Default Wert für die Zeit je Frage
+  int timer = 30;
+  String showTimer = "30";
+  bool cancelTimer = false;
+
+  //Ausgangsfarbe der Antwort Buttons
+  Map<String, Color> btnColor = {
+    "a": Colors.blue,
+    "b": Colors.blue,
+    "c": Colors.blue,
+    "d": Colors.blue
+  };
+
+  @override
+  void initState(){
+    startTimer();
+    super.initState();
+  }
+
+  //Funktion wechselt automatisch zur nächsten Frage wenn die Zeit abgelaufen ist.
+  void startTimer() async {
+    const oneSecond = Duration(seconds: 1);
+    Timer.periodic(oneSecond, (time){
+      setState(() {
+        if(timer < 1) {
+          time.cancel();
+          nextQuestion();
+        }else if(cancelTimer == true){
+          time.cancel();
+        }else{
+          timer = timer - 1;
+        }
+        showTimer = timer.toString();
+      });
+    });
+  }
+
+  //Funktion ändert den Fragenindex bzw. wechselt zur nächsten Frage
+  //setzt den Timer und die Button Farben für jede Frage zurück.
+  void nextQuestion(){
+    cancelTimer = false;
+    timer = 30;
+    setState(() {
+      if(index < 10){
+        index++;
+      }else{
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => ResultPageScreen(punkte: punkte, richtigeAntworten: richtigeAntworten, anzahlFragen: anzahlFragen),
+        ));
+      }
+      btnColor["a"] = Colors.blue;
+      btnColor["b"] = Colors.blue;
+      btnColor["c"] = Colors.blue;
+      btnColor["d"] = Colors.blue;
+    });
+    startTimer();
+  }
+
+  //Funktion prüft ob die ausgewählte Antwort korrekt ist.
+  void checkanswer(option){
+    anzahlFragen++;
+    if(myInput[2]["1"] == myInput[1]["1"][option]){
+      punkte = punkte + 5;
+      richtigeAntworten++;
+      colortoshow = right;
+    }else{
+      colortoshow = wrong;
+    }
+    setState(() {
+      btnColor[option] = colortoshow;
+      cancelTimer = true;
+    });
+
+    Timer(Duration(seconds: 2), nextQuestion);
+  }
+
+  //Antwort Buttons
   Widget antwortButton(option) {
     return Padding(
       padding: EdgeInsets.symmetric(
@@ -38,21 +133,22 @@ class _QuizPageState extends State<QuizPage> {
         horizontal: 20.0,
       ),
       child: MaterialButton(
-        onPressed: () {},
+        onPressed: () => checkanswer(option),
         child: Text(
-          "Antwort " + option,
+          "Antwort: " + myInput[1][index.toString()][option],
           style: TextStyle(
             fontSize: 18.0,
           ),
           maxLines: 1,
         ),
-        color: Colors.lime,
+        color: btnColor[option],
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    //SystemChrome sorgt dafür das die Orientierung immer gleich bleibt auf dem Smartphone Display.
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
     return WillPopScope(
@@ -82,19 +178,21 @@ class _QuizPageState extends State<QuizPage> {
       child: Scaffold(
         body: Column(
           children: <Widget>[
+            //Bereich für die Anzeige der Frage
             Expanded(
               flex: 3,
               child: Container(
                 padding: EdgeInsets.all(15.0),
                 alignment: Alignment.bottomLeft,
                 child: Text(
-                  "Beispiel Frage?",
+                  myInput[0][index.toString()],
                   style: TextStyle(
                     fontSize: 20.0,
                   ),
                 ),
               ),
             ),
+            //Bereich für die Anzeige der Antwortmöglichkeiten
             Expanded(
               flex: 6,
               child: Container(
@@ -109,13 +207,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
             ),
+            //Bereich für die Anzeige des Timers
             Expanded(
               flex: 1,
               child: Container(
                 alignment: Alignment.topCenter,
                 child: Center(
                   child: Text(
-                    "30",
+                    showTimer,
                     style: TextStyle(
                       fontSize: 20.0,
                       fontWeight: FontWeight.w700,
