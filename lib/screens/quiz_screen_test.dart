8298867cc4845f96da181898dbe3df0fb43b848c
Felix Maurer <felix.maurer@informatik.hs-fulda.de>
2020-01-27 17:36:07 +0100

    Json Update

---
+++ lib/screens/quiz_screen_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuizScreenTest extends StatefulWidget {
+  @override
+  _QuizScreenTestState createState() => _QuizScreenTestState();
+}
+
+class _QuizScreenTestState extends State<QuizScreenTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Test"),
+      ),
+    );
+  }
+}
